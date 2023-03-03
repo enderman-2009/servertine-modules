315f13dcceeac9225b1864f2beb48ee5201eb8f4
enderman-2009 <123119027+enderman-2009@users.noreply.github.com>
2023-03-03 16:04:37 -0500

    Create server.lua

---
+++ test/server.lua
@@ -0,0 +1,34 @@
+local settingstable = {}
+local doortable = {}
+local server = {}
+
+local module = {}
+module.name = "test"
+module.commands = {"test"}
+module.skipcrypt = {"test"}
+module.table = {["test"]=""}
+module.debug = false
+function module.init(settings, doors, serverCommands)
+  settingstable = settings
+  doortable = doors
+  server = serverCommands
+end
+
+function module.setup()
+
+end
+
+function module.message(command,data)
+  if command == "test" then
+    return true, {{["text"]="It worked!",["color"]=0xFFFFFF,["line"]=false}},false,true,"true"
+  else
+
+  end
+  return false
+end
+
+function module.piggyback(command,data)
+
+end
+
+return module
