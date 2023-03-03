6931de5daaa29e04736e871c0dd39db07d3b86a2
enderman-2009 <123119027+enderman-2009@users.noreply.github.com>
2023-03-03 16:46:14 -0500

    Update database.lua

--- test/database.lua
+++ test/database.lua
@@ -17,7 +17,7 @@ end
 module.onTouch = function()
   local thisInput = window:addChild(GUI.Input(2,2,16,1,0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "Hello world", "Placeholder text"))
   thisInput.text = userTable.test
-  If you add thisInput.onInputFinished = function()
+  thisInput.onInputFinished = function()
     userTable.test = thisInput.text
     database.update("test")
   end
