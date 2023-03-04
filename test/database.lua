d8dcba12de088f29a6d10deef8f3633a8606b861
enderman-2009 <123119027+enderman-2009@users.noreply.github.com>
2023-03-04 13:24:28 -0500

    Update database.lua

--- test/database.lua
+++ test/database.lua
@@ -6,7 +6,7 @@ local userTable
 local workspace, window, loc, database, style = table.unpack({...})
 
 module.name = "test"
-module.table = {"testBox","textDrop"}
+module.table = {"testBox1","textBox2"}
 module.debug = false
 module.config = {}
 
@@ -15,21 +15,22 @@ module.init = function(usTable)
 end
 
 module.onTouch = function()
-  local thisInput = window:addChild(GUI.input(2,2,16,1,style.passInputBack,style.passInputText,style.passInputPlaceholder,style.passInputFocusBack,style.passInputFocusText,"text","textBox")) --sets textbox
-  thisInput.text = userTable.testBox
-  thisInput.onInputFinished = function()
-    userTable.testBox = thisInput.text
-    database.update({"testBox"})
+  local box1 = window:addChild(GUI.input(2,2,16,1,style.passInputBack,style.passInputText,style.passInputPlaceholder,style.passInputFocusBack,style.passInputFocusText,"text1","textBox1")) --sets textbox
+  box1.text = userTable.testBox1
+  box1.onInputFinished = function()
+    userTable.testBox1 = box1.text
+    database.update({"testBox1"})
   end
-  local testDrop = window:addChild(GUI.comboBox(2,0,16,3,style.containerComboBack,style.containerComboText,style.containerComboArrowBack,style.containerComboArrowText))  --sets dropdown
-  for i, v in pairs(userTable.testDrop) do
-	testDrop:addItem(v)
+  local box2 = window:addChild(GUI.input(2,0,16,1,style.passInputBack,style.passInputText,style.passInputPlaceholder,style.passInputFocusBack,style.passInputFocusText,"text2","textBox2")) --sets textbox
+  box2.text = userTable.testBox2
+  box2.onInputFinished = function()
+    userTable.testBox2 = box2.text
+    database.update({"testBox2"})
   end
-  database.update({"testDrop"})
 end
 
 module.close = function()
-  return {"testBox","testDrop"}
+  return {"testBox1","testBox2"}
 end
 
 return module
