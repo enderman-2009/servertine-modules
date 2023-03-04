ded026b6f2d146c3ed7db1d400d6b3a617b24dc4
enderman-2009 <123119027+enderman-2009@users.noreply.github.com>
2023-03-04 12:34:13 -0500

    Update database.lua

--- test/database.lua
+++ test/database.lua
@@ -6,7 +6,7 @@ local userTable
 local workspace, window, loc, database, style = table.unpack({...})
 
 module.name = "test"
-module.table = {"testBox"}
+module.table = {"testBox","textDrop"}
 module.debug = false
 module.config = {}
 
@@ -15,22 +15,21 @@ module.init = function(usTable)
 end
 
 module.onTouch = function()
-  local thisInput = window:addChild(GUI.input(2,2,16,1,0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "Hello world", "Placeholder text")) --set textbox
+  local thisInput = window:addChild(GUI.input(2,2,16,1,style.passInputBack,style.passInputText,style.passInputPlaceholder,style.passInputFocusBack,style.passInputFocusText,"text","textBox")) --sets textbox
   thisInput.text = userTable.testBox
   thisInput.onInputFinished = function()
     userTable.testBox = thisInput.text
     database.update({"testBox"})
   end
-  local thisInput = window:addChild(GUI.input(2,0,16,1,0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "Hello world", "Placeholder text")) --set textbox
-  thisInput.text = userTable.testBox
-  thisInput.onInputFinished = function()
-    userTable.testDrop = thisInput.text
-    database.update({"testDrop"})
+  local testDrop = window:addChild(GUI.comboBox(2,0,16,3,style.containerComboBack,style.containerComboText,style.containerComboArrowBack,style.containerComboArrowText))  --sets dropdown
+  for i, v in pairs(userTable.testDrop)
+	testDrop:addItem(v)
   end
+  database.update({"testDrop"})
 end
 
 module.close = function()
-  return {"testmod","testmod2"}
+  return {"testBox","testDrop"}
 end
 
 return module
